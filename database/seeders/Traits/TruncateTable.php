fcc6d75666bde809151aa00e3f209db119bd23ea
Bui Tuan Kiet <kiet.bui@itbeesolutions.vn>
2023-03-23 10:18:05 +0700

    Done Fake data

---
+++ database/seeders/Traits/TruncateTable.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Database\Seeders\Traits;
+
+use Illuminate\Support\Facades\DB;
+
+trait TruncateTable {
+    protected function truncate($table): void {
+        DB::table($table)->truncate();
+    }
+}
